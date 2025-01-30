a2713379ece1790b7e4eeeff8617be064a947681
Josh Duncan <jdiskgolf02@yahoo.com>
2025-01-30 17:08:16 -0600

    Project2.Html

---
+++ main.js
@@ -0,0 +1,57 @@
+const giftsContainer = document.querySelector(".gifts-container");
+const input = document.querySelector("input");
+const button = document.querySelector(".btn");
+const trendingUrl =
+  "https://api.giphy.com/v1/gifs/trending?api_key=AtVnoZaM6IzFTqjKY2e2NPFeg7kIs9kI&limit=25";
+let searchUrl =
+  "https://api.giphy.com/v1/gifs/search?api_key=AtVnoZaM6IzFTqjKY2e2NPFeg7kIs9kI&limit=25&offset=0&q=query";
+
+// fetchs data
+const fetchingifts = async (query) => {
+  try {
+    if (query) {
+      const res = await fetch(`${searchUrl}q=${query}`);
+      removingChildren();
+      await normalizeData(res);
+      return;
+    }
+    const res = await fetch(trendingUrl);
+    await normalizeData(res);
+  } catch (error) {
+    console.log(error);
+  }
+};
+// create Dom elements to be shown
+const showGift = (currentlyShowing) => {
+  currentlyShowing.forEach((item, i) => {
+    const imgContainer = document.createElement("div");
+    const imgElement = document.createElement("img");
+    imgContainer.classList.add("img-container");
+    imgElement.src = item.images.downsized_medium.url;
+    imgElement.alt = item.title;
+    imgContainer.append(imgElement);
+    giftsContainer.append(imgContainer);
+  });
+};
+// remove elements previously loaded
+const removingChildren = () => {
+  Array.from(giftsContainer.children).forEach((child) => {
+    giftsContainer.removeChild(child);
+  });
+};
+
+// sets data to a json
+const normalizeData = async (res) => {
+  const resJson = await res.json();
+  showGift(resJson.data);
+};
+
+button.addEventListener("click", (e) => {
+  e.preventDefault();
+  fetchingifts(input.value);
+  input.value = "";
+});
+
+window.addEventListener("DOMContentLoaded", () => {
+  fetchingifts();
+});
---
+++ readme.md
@@ -0,0 +1,8 @@
+Josh Duncan
+The Gif Search Engine built with the help of JavaScript, HTML, and CSS. 
+It uses Giphy End Point Search API.
+
+User stories
+As a product owner, I want a clear onboarding process with intuitive navigation so I can understand how to use the app quickly.
+As a artist, I want to look up different gifs so I can get ideas on my next project.
+As a developer, I would like to create an account so I can get on and see what the app has to offer.
