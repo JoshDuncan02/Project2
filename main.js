441e769c8146ee7ec674bcbdb48833cb016efc55
Josh Duncan <Jdiskgolf02@yahoo.com>
2025-01-31 19:46:11 -0600

    Update main.js

--- main.js
+++ main.js
@@ -1,29 +1,28 @@
-const giftsContainer = document.querySelector(".gifs-container");
+const giftsContainer = document.querySelector(".gifts-container");
 const input = document.querySelector("input");
 const button = document.querySelector(".btn");
 const trendingUrl =
-  "https://api.giphy.com/v1/gifs/trending?api_key=AtVnoZaM6IzFTqjKY2e2NPFeg7kIs9kI&limit=25";
-
+  "https://api.giphy.com/v1/gifs/trending?api_key=AtVnoZaM6IzFTqjKY2e2NPFeg7kIs9kI&limit=25&rating=g";
 let searchUrl =
-  "https://api.giphy.com/v1/gifs/search?api_key=AtVnoZaM6IzFTqjKY2e2NPFeg7kIs9kI&limit=25&offset=0";
+  "https://api.giphy.com/v1/gifs/search?api_key=AtVnoZaM6IzFTqjKY2e2NPFeg7kIs9kI&limit=25&offset=0&rating=g&lang=en&";
 
 // fetchs data
-const fetchingifs = async (query) => {
+const fetchingifts = async (query) => {
   try {
     if (query) {
-      const res = await fetch(`${searchUrl}&q=${query}`);
+      const res = await fetch(`${searchUrl}q=${query}`);
       removingChildren();
-      normalizeData(res);
+      await normalizeData(res);
       return;
     }
     const res = await fetch(trendingUrl);
-    normalizeData(res);
+    await normalizeData(res);
   } catch (error) {
     console.log(error);
   }
 };
 // create Dom elements to be shown
-const showGifs = (currentlyShowing) => {
+const showGift = (currentlyShowing) => {
   currentlyShowing.forEach((item, i) => {
     const imgContainer = document.createElement("div");
     const imgElement = document.createElement("img");
@@ -36,23 +35,23 @@ const showGifs = (currentlyShowing) => {
 };
 // remove elements previously loaded
 const removingChildren = () => {
-  Array.from(gifsContainer.children).forEach((child) => {
-    gifsContainer.removeChild(child);
+  Array.from(giftsContainer.children).forEach((child) => {
+    giftsContainer.removeChild(child);
   });
 };
 
 // sets data to a json
 const normalizeData = async (res) => {
-  const resJson = res.json();
-  showGif(resJson.data);
+  const resJson = await res.json();
+  showGift(resJson.data);
 };
 
 button.addEventListener("click", (e) => {
   e.preventDefault();
-  fetchingifs(input.value);
+  fetchingifts(input.value);
   input.value = "";
 });
 
 window.addEventListener("DOMContentLoaded", () => {
-  fetchingifs();
+  fetchingifts();
 });
